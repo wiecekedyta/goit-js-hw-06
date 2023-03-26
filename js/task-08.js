141a2f5e9d6a34fee11b63eb891beb6c420429f9
wiecekedyta <wiecekedyta@gmail.com>
2023-03-26 19:12:59 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,14 @@
 
+const inputEl = document.querySelector(".login-form");
+const submitHandler = (event) => {
+    event.preventDefault();
+    const {
+        elements: { email, password },
+    } = event.currentTarget;
+    if (email.value === "" || password.value === "") {
+        return alert("Wszystkie pola muszą być uzupełnione");
+    }
+    console.log(`Email: ${email.value}, Password: ${password.value}`);
+    event.currentTarget.reset();
+}
+inputEl.addEventListener("submit", submitHandler);
