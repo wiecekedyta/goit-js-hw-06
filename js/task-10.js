6e4e96cd9055e326d366cff7f7c9de1037fe05ed
wiecekedyta <wiecekedyta@gmail.com>
2023-03-26 19:25:59 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,44 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const createBtn = document.querySelector("button[data-create]");
+const destroyBtn = document.querySelector("button[data-destroy]");
+const divBox = document.querySelector("#boxes");
+const firstdiv = document.createElement("div");
+
+let size = 20;
+
+firstdiv.style.backgroundColor = getRandomHexColor();
+
+const getInputValue = () => {
+  for (let i = 0; i < document.querySelector("input").value; i++) {
+    const divEl = document.createElement("div");
+    divBox.append(divEl);
+    divEl.setAttribute(
+      "style",
+      "width: " +
+        (size += 10) +
+        "px;" +
+        " height: " +
+        size +
+        "px;" +
+        " background-color: " +
+        getRandomHexColor()
+    );
+  }
+};
+
+const removeAct = () => {
+  const list = document.getElementById("boxes");
+  const input = document.querySelector('input[type="number"]');
+  // while (list.hasChildNodes()) {
+  //   list.removeChild(list.firstChild);
+  list.innerHTML = '';
+    size = 20;
+    input.value = '';
+  }
+
+
+
+createBtn.addEventListener("click", getInputValue);
+destroyBtn.addEventListener("click", removeAct);
