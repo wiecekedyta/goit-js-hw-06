8c96dfe034edac23e56a32e2873b552c4c3d6fa3
wiecekedyta <wiecekedyta@gmail.com>
2023-03-26 19:15:52 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,15 @@
+
+
+const btn = document.querySelector(".change-color");
+const colorCode = document.querySelector(".color");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+btn.addEventListener("click", () => {
+  colorCode.innerHTML = getRandomHexColor();
+  document.body.style.backgroundColor = getRandomHexColor();
+});
